ff0e0b8549cae47aa87fb8b0baae468d7c3b1f05
ItsMCB <33915290+ItsMCB@users.noreply.github.com>
2023-11-01 17:18:45 -0500

    Status command slight improvement

--- src/main/java/me/itsmcb/drusk/features/status/PlayerSCMD.java
+++ src/main/java/me/itsmcb/drusk/features/status/PlayerSCMD.java
@@ -1,12 +1,15 @@
 package me.itsmcb.drusk.features.status;
 
+import com.destroystokyo.paper.ClientOption;
 import me.itsmcb.drusk.Drusk;
 import me.itsmcb.vexelcore.bukkit.api.command.CustomCommand;
 import me.itsmcb.vexelcore.bukkit.api.text.BukkitMsgBuilder;
 import me.itsmcb.vexelcore.bukkit.api.utils.PluginUtils;
+import me.itsmcb.vexelcore.bukkit.plugin.CachedPlayer;
 import me.itsmcb.vexelcore.common.api.command.CMDHelper;
 import me.itsmcb.vexelcore.common.api.utils.TimeUtils;
 import net.coreprotect.CoreProtectAPI;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.ClickEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -34,24 +37,43 @@ public class PlayerSCMD extends CustomCommand {
         if (cmdHelper.argExists(0)) {
             OfflinePlayer selectedPlayer = Bukkit.getOfflinePlayer(args[0]);
             if (!(selectedPlayer.hasPlayedBefore())) {
+                new BukkitMsgBuilder("&cPlayer hasn't played before!").send(player);
                 return;
             }
+
+            TextComponent bar = new BukkitMsgBuilder("&7==========").get();
+
+            // Initial information
+            TextComponent username = new BukkitMsgBuilder("&d"+selectedPlayer.getName()).hover("&7Click to copy").clickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, selectedPlayer.getName()).get();
+            TextComponent uuid = new BukkitMsgBuilder("&7 (&5"+selectedPlayer.getUniqueId()+"&7) ").hover("&7Click to copy").clickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD,selectedPlayer.getUniqueId()+"").get();
+            player.sendMessage(bar.append(username).append(uuid.append(bar)));
+            // History
             new BukkitMsgBuilder(
-                    "\n&e&l"+
-                            selectedPlayer.getName() + " &7- &e&l" +
-                            TimeUtils.formatSecondsToTime(selectedPlayer.getStatistic(Statistic.TOTAL_WORLD_TIME)/20)
+                    "\n&7Playtime: &d"+ TimeUtils.formatSecondsToTime(selectedPlayer.getStatistic(Statistic.TOTAL_WORLD_TIME)/20)
             ).send(player);
-            new BukkitMsgBuilder("&7- UUID: &e&l"+selectedPlayer.getUniqueId())
-                    .hover("&7Click to copy")
-                    .clickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD,selectedPlayer.getUniqueId()+"").send(player);
+            // Online information
             if (selectedPlayer instanceof Player onlinePlayer) {
-                new BukkitMsgBuilder(
-                        "&7- &e" + onlinePlayer.getClientBrandName() + "&7, &e" + onlinePlayer.getPing()+"ms"
+                new BukkitMsgBuilder("&eClient Information").hover(
+                        "&7Brand Name: &d"+onlinePlayer.getClientBrandName()+"\n"+
+                                "Ping: "+onlinePlayer.getPing()+"\n"+
+                                "View Distance: "+onlinePlayer.getClientViewDistance()+"\n"+
+                                "Locale: "+onlinePlayer.getClientOption(ClientOption.LOCALE)+"\n"+
+                                "Chat Visibility: "+onlinePlayer.getClientOption(ClientOption.CHAT_VISIBILITY).name()+"\n"+
+                                "Chat Colors Enabled: "+onlinePlayer.getClientOption(ClientOption.CHAT_COLORS_ENABLED)+"\n"+
+                                "Allow Server Listings: "+onlinePlayer.getClientOption(ClientOption.ALLOW_SERVER_LISTINGS)+"\n"+
+                                "Main Hand: "+onlinePlayer.getClientOption(ClientOption.MAIN_HAND)+"\n"+
+                                "Text Filtering Enabled: "+onlinePlayer.getClientOption(ClientOption.TEXT_FILTERING_ENABLED)+"\n"+
+                                "Has Cape: "+onlinePlayer.getClientOption(ClientOption.SKIN_PARTS).hasCapeEnabled()
                 ).send(player);
-                new BukkitMsgBuilder("&7- &eIP").hover("&7"+onlinePlayer.getAddress().getAddress()).send(player);
             }
-            new BukkitMsgBuilder("&7- &aFirst Played " + TimeUtils.concise2EpochDateFromMilliseconds(selectedPlayer.getFirstPlayed())).send(player);
-            new BukkitMsgBuilder("&7- &cLast Seen " + TimeUtils.concise2EpochDateFromMilliseconds(selectedPlayer.getLastSeen())+"\n").send(player);
+            new BukkitMsgBuilder("&7- &aFirst Played: " + TimeUtils.concise2EpochDateFromMilliseconds(selectedPlayer.getFirstPlayed())).send(player);
+            new BukkitMsgBuilder("&7- &cLast Seen: " + TimeUtils.concise2EpochDateFromMilliseconds(selectedPlayer.getLastSeen())+"\n").send(player);
+
+            CachedPlayer cachedPlayer = new CachedPlayer(selectedPlayer.getUniqueId());
+            String value = cachedPlayer.getPlayerSkin().getValue();
+            new BukkitMsgBuilder("&dSkin Value").hover("&7"+value).clickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD,value).send(player);
+            String signature = cachedPlayer.getPlayerSkin().getSignature();
+            new BukkitMsgBuilder("&dSkin Signature").hover("&7"+signature).clickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD,signature).send(player);
 
             // CoreProtect extension
             CoreProtectAPI coreProtectAPI = PluginUtils.getCoreProtect();
@@ -70,10 +92,8 @@ public class PlayerSCMD extends CustomCommand {
             sendCoreProtectLookupFormatted(blocksPlacedLookup, player);
             new BukkitMsgBuilder("&7- &eInteractions:").hover("View more logs").clickEvent(ClickEvent.Action.SUGGEST_COMMAND,"/co lookup "+selectedPlayer.getName()+" time:1w ").send(player);
             sendCoreProtectLookupFormatted(interactionLookup, player);
-            return;
         }
         // instance.getServer().getServicesManager().load(LuckPerms.class).getUserManager().getUser(selectedPlayer.getUniqueId()).getPrimaryGroup() + " &7- &e&l" +
-        new BukkitMsgBuilder("Please provide a valid username!").send(player);
     }
 
     @Override
